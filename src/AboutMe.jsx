25462786ed12e0035a47e21502a28e33d17e44ac
cliffrd1 <lagataclifford@gmail.com>
2025-02-17 23:25:57 +0800

    Added new components and updated styles

---
+++ src/AboutMe.jsx
@@ -0,0 +1,21 @@
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <p>
+      I am an Information Technology student who is passionate
+      in the IT Industry and explore various fields of technology. I
+      am committed to delivering excellent customer service and
+      ensuring efficient operations. With a strong passion for
+      technology and a talent for troubleshooting, I specialize in
+      both software and hardware solutions.
+      </p>
+
+      <h2> Phone Number & Email</h2>
+      <p>09686856904 | lagataclifford@gmail.com</p>
+    </div>
+  )
+}
+
+export default AboutMe
+
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card"
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="hero-content">
+          <h1>Welcome to My Portfolio</h1>
+          <p>I love Information Technology</p>
+        </div>
+        <Card />
+      </div>
+    </div>
+  )
+}
+
+export default Home
+
